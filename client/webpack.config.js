3c8528a423b2a9e2cb8f78d75e0513666a2ba480
Ricardo Pessoa <ricardoandre.pessoa@gmail.com>
2020-11-01 15:48:20 -0300

    final

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,60 +3,85 @@ const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWepackPlugin = require('html-webpack-plugin');
 
 let plugins = [];
 
-plugins.push(
+plugins.push(new HtmlWepackPlugin({
 
-    new extractTextPlugin("styles.css")
+    hash: true,
+    minify: {
+        html5: true, 
+        collapseWhitespace: true, 
+        removeComments: true
+    },
+    filename: 'index.html',
+    template: __dirname + '/main.html'
 
-);
+}));
+plugins.push(new extractTextPlugin('styles.css'))
 
-plugins.push(
-    new webpack.ProvidePlugin({
-           '$': 'jquery/dist/jquery.js',
-           'jQuery': 'jquery/dist/jquery.js'
-    })
-);
+plugins.push(new webpack.ProvidePlugin({
+    '$': 'jquery/dist/jquery.js',
+    'jQuery': 'jquery/dist/jquery.js'
+}));
+
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+
+    name: 'vendor', 
+    filename: 'vendor.bundle.js'
+
+}));
+
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
 
 if(process.env.NODE_ENV == 'production') {
+
+    SERVICE_URL = JSON.stringify('http://endereco-da-sua-api');
+
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
 
     plugins.push(new babiliPlugin());
 
     plugins.push(new optimizeCSSAssetsPlugin({
         cssProcessor: require('cssnano'),
-        cssProcessorOptions: { 
+        cssProcessorOptions: {
             discardComments: {
-                removeAll: true 
+                removeAll: true
             }
         },
         canPrint: true
-     }));    
-
+    }));
+        
 }
 
+plugins.push(new webpack.DefinePlugin({ SERVICE_URL }));
+
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap','reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: "dist"
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: {
+                use : { 
                     loader: 'babel-loader'
                 }
             },
-            { 
-                test: /\.css$/, 
+            {
+                test: /\.css$/,
                 use: extractTextPlugin.extract({
-                    fallback: 'style-loader',
+                    fallback: 'style-loader', 
                     use: 'css-loader'
                 })
+
             },
             { 
                 test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
@@ -73,8 +98,8 @@ module.exports = {
             { 
                 test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
                 loader: 'url-loader?limit=10000&mimetype=image/svg+xml' 
-            }            
+            }             
         ]
-    },
+    }, 
     plugins
 }
